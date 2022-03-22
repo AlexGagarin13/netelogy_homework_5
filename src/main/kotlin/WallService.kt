8b0ece12180a7d21ed3673717926c6862b8564cf
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-03-22 16:36:34 +0300

    add method added

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,10 +1,22 @@
 object WallService {
     private var posts = emptyArray<Post>()
+    private var PostId: Int = 0
 
-    fun add(post: Post): Post{
-        TODO()
+    private fun generateId(): Int {
+        PostId += 1
+        return PostId
     }
-    fun update(post: Post): Boolean {
-        TODO()
+
+    fun add(post: Post): Post {
+        val post = post.copy(id = generateId())
+        posts += post
+        return posts.last()
+    }
+
+//    fun update(post: Post): Boolean {
+//        for ((index, post) in posts.withIndex()) {
+//            if (post.id = id)
+//        }
+//        return false
     }
 }
